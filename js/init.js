9974dc9eccb1e3d23ff0befb02f293d2f06ed7ff
Wliu <Wliu1402@gmail.com>
2015-05-12 17:42:26 -0400

    :shirt: Clean up some lint warnings

--- js/init.js
+++ js/init.js
@@ -1,25 +1,32 @@
+"use strict";
 var canvas;
 var context;
-var scene;
+
 $(document).ready(function(){
-  $("<canvas/>" ).attr({
-    id: "main_canvas",width:$(document).innerWidth()+"px",height: $(document).innerHeight()+"px"
-  }).css({
-    background: "#7ec0ee"
-  }).appendTo("#main_container");
-  canvas = document.getElementById("main_canvas");
-  context = canvas.getContext("2d");
-  StartMainMenu();
+    $("<canvas/>").attr({
+        id: "main_canvas", width:$(document).innerWidth()+"px", height: $(document).innerHeight()+"px"
+    }).css({
+        background: "#7ec0ee"
+    }).appendTo("#main_container");
+    canvas = document.getElementById("main_canvas");
+    context = canvas.getContext("2d");
+    StartMainMenu();
 });
 
-function StartMainMenu(){
-  //Creating a new Menu Object
-  var MainMenu = new Menu("The Battle of Calculus Planes",
-  [ "Play", "Settings"],
-  200, 50, 200,
-  function(numItem) { if (numItem == 0) StartGame(); });
-  GameLoopManager.run(function(elapsed) { MainMenu.Tick(elapsed);});
+function StartMainMenu()
+{
+    //Creating a new Menu Object
+    var MainMenu = new Menu("The Battle of Calculus Planes",
+    [ "Play", "Settings"],
+    200, 50, 200,
+    function(numItem)
+    {
+        if(numItem === 0)
+        {
+            StartGame();
+        }
+    });
+    GameLoopManager.run(function(){MainMenu.Tick();});
 
-  document.addEventListener("mousedown", function(e){MainMenu.mouseDown(e)}, false);
-
+    document.addEventListener("mousedown", function(e){MainMenu.mouseDown(e);}, false);
 }
--- js/menu.js
+++ js/menu.js
@@ -1,59 +1,66 @@
-Menu = function(title, items, y, size, width, callback)
+"use strict";
+var Menu = function(title, items, y, size, width, callback)
 {
-  this.title = title;
-  this.items = items;
-  this.selectedItem = 0;
-  this.callback = callback;
-  this.y = y;
-  this.size = size;
-  this.width = width;
-  var floatingManager = new FloatingObjectManager();
-  floatingManager.createObject("images/menu_airplane.png", 4);
-  floatingManager.move();
-}
+    this.title = title;
+    this.items = items;
+    this.selectedItem = 0;
+    this.callback = callback;
+    this.y = y;
+    this.size = size;
+    this.width = width;
+    var floatingManager = new FloatingObjectManager();
+    floatingManager.createObject("images/menu_airplane.png", 4);
+    floatingManager.move();
+};
 
 Menu.prototype.constructor = Menu;
 
-Menu.prototype.Render = function(){
-  //Gradient look
-  var lingrad = context.createLinearGradient(0,0,0,canvas.height);
-  lingrad.addColorStop(0, '#add8e6');
-  lingrad.addColorStop(1, '#259');
-  context.fillStyle = lingrad;
-  //context.fillRect(0,0,canvas.width, canvas.height);
+Menu.prototype.Render = function()
+{
+    //Gradient look
+    var lingrad = context.createLinearGradient(0, 0, 0, canvas. height);
+    lingrad.addColorStop(0, '#add8e6');
+    lingrad.addColorStop(1, '#259');
+    context.fillStyle = lingrad;
+    //context.fillRect(0,0,canvas.width, canvas.height);
 
-  context.textAlign = "center";
-  context.fillStyle = "White";
+    context.textAlign = "center";
+    context.fillStyle = "White";
 
-  var y = this.y;
-  if (this.title){
-    //Blinking text looks cool :P
-    //var v = Math.floor(127*Math.cos(GameLoopManager.lastTime*0.04) + 127);
-    context.fillStyle = "#FFCCCB";
-    context.font = Math.floor(this.size*1.3).toString() + "px Arial";
-    context.fillText(this.title, canvas.width/2, y);
-    y += this.size;
-  }
+    var y = this.y;
+    if (this.title)
+    {
+        //Blinking text looks cool :P
+        //var v = Math.floor(127*Math.cos(GameLoopManager.lastTime*0.04) + 127);
+        context.fillStyle = "#FFCCCB";
+        context.font = Math.floor(this.size*1.3).toString() + "px Arial";
+        context.fillText(this.title, canvas.width/2, y);
+        y += this.size;
+    }
 
-  for (var i = 0; i < this.items.length; ++i){
-    var size = Math.floor(this.size*0.8);
-    context.fillStyle = "white";
-    size = this.size;
-    context.font = size.toString() + "px Arial";
-    y += this.size;
-    context.fillText(this.items[i], canvas.width/2, y);
-  }
-}
+    for (var i = 0; i < this.items.length; ++i)
+    {
+        var size = Math.floor(this.size*0.8);
+        context.fillStyle = "white";
+        size = this.size;
+        context.font = size.toString() + "px Arial";
+        y += this.size;
+        context.fillText(this.items[i], canvas.width/2, y);
+    }
+};
 
-Menu.prototype.mouseDown = function(e){
-  canvas_x = e.pageX;
-  canvas_y = e.pageY;
-  if(canvas_x > canvas.width/2-this.width/2 && canvas_x < canvas.width/2+this.width/2){
-    //I'm probably going to make a seperate class for buttons, but this will do for now
-    alert("clicked (horizontal check only for now, fixing later)");
-  }
-}
+Menu.prototype.mouseDown = function(e)
+{
+    var canvas_x = e.pageX;
+    var canvas_y = e.pageY;
+    if(canvas_x > canvas.width/2-this.width/2 && canvas_x < canvas.width/2+this.width/2)
+    {
+        //I'm probably going to make a seperate class for buttons, but this will do for now
+        alert("clicked (horizontal check only for now, fixing later)");
+    }
+};
 
-Menu.prototype.Tick = function(elapsed){
-  this.Render();
-}
+Menu.prototype.Tick = function()
+{
+    this.Render();
+};

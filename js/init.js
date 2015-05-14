a7280d073a83087d194173cd49f01e18f04f1cbd
saxocellphone <victor741naruto@gmail.com>
2015-05-14 17:04:52 -0400

    Changes

--- js/init.js
+++ js/init.js
@@ -26,6 +26,8 @@ function StartMainMenu()
 			StartGame();
 		}
 	});
+	var menu = buildHtml($.getJSON("json/welcome_menu.json"));
+	$("body").append(menu);
 	//GameLoopManager.run(function(){MainMenu.Tick();});
 
 	//document.addEventListener("mousedown", function(e){MainMenu.mouseDown(e);}, false);

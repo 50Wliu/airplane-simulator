eaf93947bffcfff12e2726b60a4adcdd1ed99997
saxocellphone <victor741naruto@gmail.com>
2015-05-13 22:01:21 -0400

    Different Speed

--- js/FloatingObject.js
+++ js/FloatingObject.js
@@ -1,34 +1,39 @@
 "use strict";
 
-function FloatingObject(source, x, y, scale)
+function FloatingObject(source, x, y, scale, speed)
 {
 	this.source = source;
 	this.x = x;
 	this.y = y;
 	this.scale = scale;
+  this.speed = speed;
 	this.image = new Image();
 	var bindThis = this;  //Convince onload that we actually mean FloatingObject
 	this.image.onload = function()
 	{
-		bindThis.width = bindThis.image.width * scale;
-		bindThis.height = bindThis.image.height * scale;
+		bindThis.width = Math.round(bindThis.image.width * scale);
+		bindThis.height = Math.round(bindThis.image.height * scale);
 	};
 	this.image.src = source;
 }
 
 FloatingObject.prototype.draw = function()
 {
-	context.clearRect(this.x - 2, this.y, this.width, this.height);  //FIXME: Figure out why this leaves a rendering artifact using `this.x - 1`
-	context.drawImage(this.image, this.x, this.y, this.width, this.height);
+
+	context.clearRect(this.x - 5, this.y, this.width, this.height);  //FIXME: Figure out why this leaves a rendering artifact using `this.x - 1`
+  context.drawImage(this.image, this.x, this.y, this.width, this.height);
+  console.log(this.x);
 };
 
-FloatingObject.prototype.setCoords = function(x, y)
+FloatingObject.prototype.setCoords = function(x, y, speed)
 {
 	if(x >= canvas.width)  //The object has passed the edge of the canvas, so reset its path
 	{
 		x = -this.width;  //We want the object to move in gracefully instead of just reappearing
 		y = Math.round(Math.random() * canvas.height);  //Randomize the height
+    speed = Math.round(Math.random() * 4 + 1);
 	}
 	this.x = x;
 	this.y = y;
+  this.speed = speed;
 };
--- js/FloatingObjectManager.js
+++ js/FloatingObjectManager.js
@@ -9,7 +9,7 @@ FloatingObjectManager.prototype.createObject = function(source, amount)
 {
 	for(var i = 0; i < amount; i++)
 	{
-		this.objectArray.push(new FloatingObject(source, Math.round(Math.random()*canvas.width), Math.round(Math.random()*canvas.height), 0.3));
+		this.objectArray.push(new FloatingObject(source, Math.round(Math.random()*canvas.width), Math.round(Math.random()*canvas.height), 0.3, Math.round(Math.random()*4+1)));
 	}
 };
 
@@ -20,8 +20,10 @@ FloatingObjectManager.prototype.move = function()
 	{
 		for(var i = 0; i < bindThis.objectArray.length; i++)
 		{
-			bindThis.objectArray[i].setCoords(bindThis.objectArray[i].x + 1, bindThis.objectArray[i].y);
-			bindThis.objectArray[i].draw();
+
+			bindThis.objectArray[i].setCoords(bindThis.objectArray[i].x + bindThis.objectArray[i].speed, bindThis.objectArray[i].y, bindThis.objectArray[i].speed);
+      bindThis.objectArray[i].draw();
+
 		}
 	}, 1000 / 30);
 };

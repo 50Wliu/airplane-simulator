c5bc7863fa411a860e6c8dc8412704218438428a
Wliu <Wliu1402@gmail.com>
2015-05-16 14:46:52 -0400

    Remove a whole lot of stuff that isn't needed anymore

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,5 @@
 "use strict";
-var Menu = function()
-{
-	var floatingManager = new FloatingObjectManager();
-	floatingManager.createObject("images/menu_airplane.png", 4);
-	floatingManager.move();
-};
-
-
-Menu.prototype.create = function(){
-	$.getJSON("json/welcome_menu.json", function (data) {
-		console.log(data);
-		$("body").append(buildHtml(data));
-	}).fail(function(){
-		console.log("error: what the heck went wrong? Ugh now I have to go back and debug it :(");
-	});
-}
-
-Menu.prototype.remove = function(id)
+function remove(id)
 {
 	$(document.getElementById(id)).remove();
-};
+}

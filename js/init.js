a857047b4cb4421f65e90fbc5304a89642d96284
Wliu <Wliu1402@gmail.com>
2015-05-21 20:31:33 -0400

    Remove main_container and append the canvas directly to the body main_canvas has also been renamed to canvas since I highly doubt there's going to be multiple canvases...

--- js/init.js
+++ js/init.js
@@ -5,28 +5,19 @@ var menu;
 $(document).ready(function()
 {
 	$('[data-toggle="tooltip"]').tooltip();  //Enable Bootstrap tooltips
+
 	$("#form").submit(function(event)
 	{
-		event.preventDefault();  //Prevent forum submissions from refreshing the page
+		event.preventDefault();  //Prevent form submissions from refreshing the page
 	});
 
 	$("<canvas/>").attr(
 	{
-		id: "main_canvas", width:$(document).innerWidth()+"px", height: $(document).innerHeight()+"px"
-	}).css(
-	{
-		background: "#add8e6"
-	}).appendTo("#main_container");
+		id: "canvas", width: $(document).innerWidth()+"px", height: $(document).innerHeight()+"px"
+	}).appendTo("body");
 
-	canvas = document.getElementById("main_canvas");
+	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
 	menu = new Menu();
 	//Making Menu a seperate class just incase we want to add more to it
 });
-
-function startMainGame(nickname){
-	menu.remove();
-	$(document.getElementById("overlays")).remove();
-	$(document.getElementById("main_canvas")).remove();
-	var main_game = new MainGame(nickname);
-}
--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,22 @@
 "use strict";
-function Menu(){
+function Menu()  //TODO: Move the FloatingObject stuff to a different function/file
+{
 	var floatingManager = new FloatingObjectManager();
 	floatingManager.createObject("images/menu_airplane.png", 4);
 	floatingManager.move();
 	this.floatingManager = floatingManager;
 }
-Menu.prototype.remove = function(){
+
+Menu.prototype.remove = function()
+{
 	this.floatingManager.stop();
-	context.clearRect(0,0,canvas.width, canvas.height);
+	context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+function startGame(nickname)  //TODO: Refactor this so that it doesn't rely on init.js's 'menu' variable
+{
+	menu.remove();
+	$(document.getElementById("overlays")).remove();
+	$(document.getElementById("canvas")).remove();
+	var game = new MainGame(nickname);
 }

8880a5145b0de5b542aa8bcc50ab5b0504f8098f
50Wliu <Wliu1402@gmail.com>
2015-06-01 14:08:34 -0400

    Change the plane back to the old one Also ceil this.width and this.height in .clearRect...

--- js/FloatingObject.js
+++ js/FloatingObject.js
@@ -21,7 +21,7 @@ function FloatingObject(source, x, y, scale, speed)
 FloatingObject.prototype.draw = function()
 {
 	//We floor the x and y values here because .clearRect can't reliably erase parts of a pixel
-	context.clearRect(Math.floor(this.x - this.speed), Math.floor(this.y), this.width, this.height);
+	context.clearRect(Math.floor(this.x - this.speed), Math.floor(this.y), Math.ceil(this.width), Math.ceil(this.height));
 	context.drawImage(this.image, this.x, this.y, this.width, this.height);
 };
 
--- js/init.js
+++ js/init.js
@@ -47,7 +47,7 @@ $(window).ready(function()
 
 	//Background airplanes!
 	floatingManager = new FloatingObjectManager();
-	floatingManager.createObject("images/menu_airplane1.png", 4);
+	floatingManager.createObject("images/menu_airplane.png", 4);
 	floatingManager.move();
 });
 
